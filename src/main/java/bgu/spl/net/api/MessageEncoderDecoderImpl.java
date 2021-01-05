3c5e581e6355ea7062b9941e39cc43e534da88a6
bar dabun <bardabun@gmail.com>
2021-01-06 00:26:57 +0200

    EncoderDecoder

--- src/main/java/bgu/spl/net/api/MessageEncoderDecoderImpl.java
+++ src/main/java/bgu/spl/net/api/MessageEncoderDecoderImpl.java
@@ -7,13 +7,34 @@ public class MessageEncoderDecoderImpl<T> implements MessageEncoderDecoder<Strin
 
     private byte[] bytes = new byte[1 << 10]; //start with 1k
     private int len = 0;
-    int opcodeFlag = 0;
+    int numOfZeros = 0;
 
     @Override
     public String decodeNextByte(byte nextByte) {
-        if(nextByte == '\0'){
-            short opCode = bytesToShort(bytes);
+        if(len >= 2) {
+            byte[] tmp = new byte[2];
+            tmp[0]= bytes[0];
+            tmp[1]=bytes[1];
+            short opCode = bytesToShort(tmp);
+            if (nextByte == '\0')
+                numOfZeros++;
+            if (len == 2) {
+                opCode = bytesToShort(bytes);
+                if (opCode == 4 | opCode == 11) {
+                    return popString();
+                }
+            }
+            //between 1 to 3
+            if (opCode >= 1 & opCode <= 3 & numOfZeros == 3)
+                return popString();
 
+            if ((opCode >= 5 & opCode <= 10 & opCode != 8 & numOfZeros == 1))
+                return popString();
+
+            if (opCode == 8 & numOfZeros == 2)
+                return popString();
+
+        }
         pushByte(nextByte);
         return null;
     }
