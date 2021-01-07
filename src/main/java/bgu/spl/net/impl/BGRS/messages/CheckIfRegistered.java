02529b12278db08b5dc9a14ed94017e63b57e5ef
bar dabun <bardabun@gmail.com>
2021-01-07 23:02:55 +0200

    Check my current courses, Unregister to course

--- src/main/java/bgu/spl/net/impl/BGRS/messages/CheckIfRegistered.java
+++ src/main/java/bgu/spl/net/impl/BGRS/messages/CheckIfRegistered.java
@@ -1,4 +1,18 @@
 package bgu.spl.net.impl.BGRS.messages;
 
-public class CheckIfRegistered {
+import bgu.spl.net.impl.BGRS.Database;
+import bgu.spl.net.impl.BGRS.messages.BasicMessage.MessageUsername;
+
+public class CheckIfRegistered extends MessageUsername {
+    private String username;
+    private int courseName;
+    private int opCode;
+    public CheckIfRegistered(String username, int courseNumber) {
+
+    }
+
+    @Override
+    public String execute() {
+        return null;
+    }
 }
--- src/main/java/bgu/spl/net/impl/BGRS/messages/UnregisterToCourse.java
+++ src/main/java/bgu/spl/net/impl/BGRS/messages/UnregisterToCourse.java
@@ -1,22 +1,16 @@
 package bgu.spl.net.impl.BGRS.messages;
 
-import bgu.spl.net.impl.BGRS.Database;
-import bgu.spl.net.impl.BGRS.User;
+import bgu.spl.net.impl.BGRS.messages.BasicMessage.MessageUsernameCourseumber;
 
-public class UnregisterToCourse {
-    private String username;
-    private int courseNumber;
-    private int opCode;
-    private Database DB = Database.getInstance();
+public class UnregisterToCourse extends MessageUsernameCourseumber {
 
     public UnregisterToCourse(String username, int courseNumber) {
-        this.username = username;
-        this.courseNumber = courseNumber;
-        this.opCode = 10;
+        super(username, courseNumber);
+        this.opcode = 10;
     }
 
     public String execute() {
-        return DB.unregisterToCourse(username, courseNumber) ?
-                new Acknowledgement(opCode).execute(): new Error(opCode).execute();
+        return DB.unregisterToCourse(userName, courseNumber) ?
+                new Acknowledgement(opcode).execute(): new Error(opcode).execute();
     }
 }
